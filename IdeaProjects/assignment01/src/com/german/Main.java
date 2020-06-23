f013d96825418ef5b533ab955f21fb0f413aff74
gguzmangithub <gguzman@g.harvard.edu>
2020-06-23 08:44:48 -0400

    change x
    Another change to organize code a little bit

--- IdeaProjects/assignment01/src/com/german/Main.java
+++ IdeaProjects/assignment01/src/com/german/Main.java
@@ -9,7 +9,7 @@ public class Main {
     }
 }
 
-//Command to execute in terminal for the program above
+//Command to execute in terminal for the program above. it starts after C:\German\Harvard\CSCI71\GitHub\Repositories\csci71assignment01\IdeaProjects\assignment01>
 //C:\German\Harvard\CSCI71\GitHub\Repositories\csci71assignment01\IdeaProjects\assignment01>C:\German\Harvard\CSCI71\OpenJDK\installfile\openjdk-14_windows-x64_bin\jdk-14\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA 2020.1.2\lib\idea_rt.jar=64349:C:\Program Files\JetBrains\IntelliJ IDEA 2020.1.2\bin" -Dfile.encoding=UTF-8 -classpath C:\German\Harvard\CSCI71\GitHub\Repositories\csci71assignment01\IdeaProjects\assignment01\out\production\assignment01 com.german.Main Pepe
 
 
