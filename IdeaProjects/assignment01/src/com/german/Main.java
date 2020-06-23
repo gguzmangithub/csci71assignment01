6d42022ff0be63b07ed7287cebab74c089b776bc
gguzmangithub <gguzman@g.harvard.edu>
2020-06-23 01:35:17 -0400

    Assignment one
    This is Assignment One

---
+++ IdeaProjects/assignment01/src/com/german/Main.java
@@ -0,0 +1,11 @@
+package com.german;
+
+public class Main {
+
+    public static void main(String args[]) {
+	// write your code here
+        System.out.println("Hello, "+args[0]+"!");
+      ///  System.out.println("Hello, !");
+    }
+}
+
