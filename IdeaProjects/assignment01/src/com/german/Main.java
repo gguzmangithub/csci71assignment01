6610bda7973e7a28cb447bbec16627e70414cbad
gguzmangithub <gguzman@g.harvard.edu>
2020-06-23 01:56:47 -0400

    Change2
    more changes

--- IdeaProjects/assignment01/src/com/german/Main.java
+++ IdeaProjects/assignment01/src/com/german/Main.java
@@ -9,3 +9,10 @@ public class Main {
     }
 }
 
+//Terminal Command
+//C:\German\Harvard\CSCI71\GitHub\Repositories\csci71assignment01\IdeaProjects\assignment01>C:\German\Harvard\CSCI71\OpenJDK\installfile\openjdk-14_windows-x64_bin\jdk-14\bin\java.exe "-javaagen
+//t:C:\Program Files\JetBrains\IntelliJ IDEA 2020.1.2\lib\idea_rt.jar=64349:C:\Program Files\JetBrains\IntelliJ IDEA 2020.1.2\bin" -Dfile.encoding=UTF-8 -classpath C:\German\Harvard\CSCI71\GitHu
+// b\Repositories\csci71assignment01\IdeaProjects\assignment01\out\production\assignment01 com.german.Main pepe
+
+
+
