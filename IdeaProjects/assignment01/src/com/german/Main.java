802f4b3f6315ca9d03867635ca0962f5ab604f42
gguzmangithub <gguzman@g.harvard.edu>
2020-06-23 08:13:49 -0400

    Third commit
    Adding comments to the code

--- IdeaProjects/assignment01/src/com/german/Main.java
+++ IdeaProjects/assignment01/src/com/german/Main.java
@@ -4,15 +4,13 @@ public class Main {
 
     public static void main(String args[]) {
 	// write your code here
-        System.out.println("Hello, "+args[0]+"!");
+       System.out.println("Hello, "+args[0]+"!");
       ///  System.out.println("Hello, !");
     }
 }
 
-//Terminal Command
-//C:\German\Harvard\CSCI71\GitHub\Repositories\csci71assignment01\IdeaProjects\assignment01>C:\German\Harvard\CSCI71\OpenJDK\installfile\openjdk-14_windows-x64_bin\jdk-14\bin\java.exe "-javaagen
-//t:C:\Program Files\JetBrains\IntelliJ IDEA 2020.1.2\lib\idea_rt.jar=64349:C:\Program Files\JetBrains\IntelliJ IDEA 2020.1.2\bin" -Dfile.encoding=UTF-8 -classpath C:\German\Harvard\CSCI71\GitHu
-// b\Repositories\csci71assignment01\IdeaProjects\assignment01\out\production\assignment01 com.german.Main pepe
+//Command to execute in terminal for the program above
+//C:\German\Harvard\CSCI71\GitHub\Repositories\csci71assignment01\IdeaProjects\assignment01>C:\German\Harvard\CSCI71\OpenJDK\installfile\openjdk-14_windows-x64_bin\jdk-14\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA 2020.1.2\lib\idea_rt.jar=64349:C:\Program Files\JetBrains\IntelliJ IDEA 2020.1.2\bin" -Dfile.encoding=UTF-8 -classpath C:\German\Harvard\CSCI71\GitHub\Repositories\csci71assignment01\IdeaProjects\assignment01\out\production\assignment01 com.german.Main Pepe
 
 
 
